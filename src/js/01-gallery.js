a021ec380acc83c36d8c62e5f3988b5a55711a9c
Roman Klimenko <roman.sound.klimenko@gmail.com>
2022-06-30 20:29:25 +0300

    fin

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,28 +5,28 @@ import '../css/common.css';
 import '../css/01-gallery.css';
 
 const createItemsMarkup = galleryItems
-    .map(({ preview, original, description }) => {
-      return`
-      <a class="gallery__item" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          alt="${description}"/>
-      </a> `;
-    })
-    .join('');
+  .map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+      `;
+  })
+  .join('');
 
-const galleryContainerEl = document.querySelector('.gallery');
-galleryContainerEl.insertAdjacentHTML('beforeend', createItemsMarkup);
-const lighbox = new SimpleLightbox('gallery a', {
-    scrollZoom: false,
-    captionDelay: 250,
-    captionsData: 'alt',
-    doubleTapZoom: 1,
+const alleryContainerEl = document.querySelector('.gallery');
+alleryContainerEl.insertAdjacentHTML('beforeend', createItemsMarkup);
+let lightbox = new SimpleLightbox('.gallery a', {
+  scrollZoom: false,
+  captionDelay: 250,
+  captionsData: 'alt',
+  doubleTapZoom: 1,
 });
-galleryContainerEl.addEventListener('click', event => {
+alleryContainerEl.addEventListener('click', event => {
   event.preventDefault();
   if (!event.target.classList.contains('gallery__image')) {
     return;
   }
 });
+
+//console.log(galleryItems);
